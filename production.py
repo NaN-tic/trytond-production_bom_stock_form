0caffce8cf5b2f69aeaeb7ed3e4f4792a940c7ec
Raimon Esteve <raimon@nan-tic.com>
2017-03-01 13:32:35 +0100

    Show stock option in BOM Tree

--- production.py
+++ production.py
@@ -2,6 +2,7 @@
 # copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval
 from trytond.transaction import Transaction
 from trytond.modules.stock_calculation import StockMixin
 
@@ -19,12 +20,24 @@ class BOMTree(StockMixin):
     @classmethod
     def set_stock_recursively(cls, bom_tree):
         Product = Pool().get('product.product')
-        product = Product(bom_tree['product'])
-        bom_tree['input_stock'] = cls.get_input_output_product([product],
-            'input_stock')[product.id]
-        bom_tree['output_stock'] = cls.get_input_output_product([product],
-            'output_stock')[product.id]
-        bom_tree['current_stock'] = product.quantity
+
+        if Transaction().context.get('show_stock', True):
+            product = Product(bom_tree['product'])
+
+            input_stock = cls.get_input_output_product(
+                [product], 'input_stock')[product.id]
+            output_stock = cls.get_input_output_product(
+                [product], 'output_stock')[product.id]
+            quantity = product.quantity
+        else:
+            input_stock = None
+            output_stock = None
+            quantity = None
+
+        bom_tree['input_stock'] = input_stock
+        bom_tree['output_stock'] = output_stock
+        bom_tree['current_stock'] = quantity
+
         if bom_tree['childs']:
             for child in bom_tree['childs']:
                 cls.set_stock_recursively(child)
@@ -40,11 +53,26 @@ class BOMTree(StockMixin):
 class OpenBOMTreeStart:
     __metaclass__ = PoolMeta
     __name__ = 'production.bom.tree.open.start'
-    date = fields.Date('Date', required=True)
-    warehouse = fields.Many2One('stock.location', 'Warehouse', required=True,
+    show_stock = fields.Boolean('Show stock',
+        help='Show stock quantity')
+    date = fields.Date('Date',
+        states={
+            'required': Eval('show_stock', False),
+            'invisible': ~Eval('show_stock', False),
+        }, depends=['show_stock'],
+        help='The date that calculate stock quantity')
+    warehouse = fields.Many2One('stock.location', 'Warehouse',
         domain=[
             ('type', '=', 'warehouse'),
-            ])
+        ], states={
+            'required': Eval('show_stock', False),
+            'invisible': ~Eval('show_stock', False),
+        }, depends=['show_stock'],
+        help='The warehouse that calculate stock quantity')
+
+    @staticmethod
+    def default_show_stock():
+        return True
 
 
 class OpenBOMTreeTree(StockMixin):
@@ -55,15 +83,26 @@ class OpenBOMTreeTree(StockMixin):
     def tree(cls, bom, product, quantity, uom):
         Product = Pool().get('product.product')
 
-        bom_tree = super(OpenBOMTreeTree, cls).tree(bom, product, quantity,
-            uom)
+        bom_tree = super(OpenBOMTreeTree, cls).tree(
+            bom, product, quantity, uom)
 
-        product = Product(bom_tree['bom_tree'][0]['product'])
-        bom_tree['bom_tree'][0]['input_stock'] = cls.get_input_output_product(
-            [product], 'input_stock')[product.id]
-        bom_tree['bom_tree'][0]['output_stock'] = cls.get_input_output_product(
-            [product], 'output_stock')[product.id]
-        bom_tree['bom_tree'][0]['current_stock'] = product.quantity
+        if Transaction().context.get('show_stock', True):
+            product = Product(bom_tree['bom_tree'][0]['product'])
+
+            input_stock = cls.get_input_output_product(
+                [product], 'input_stock')[product.id]
+            output_stock = cls.get_input_output_product(
+                [product], 'output_stock')[product.id]
+            quantity = product.quantity
+        else:
+            input_stock = None
+            output_stock = None
+            quantity = None
+
+        bom_tree['bom_tree'][0]['input_stock'] = input_stock
+        bom_tree['bom_tree'][0]['output_stock'] = output_stock
+        bom_tree['bom_tree'][0]['current_stock'] = quantity
+
         return bom_tree
 
 
@@ -78,6 +117,11 @@ class OpenBOMTree:
         return defaults
 
     def default_tree(self, fields):
-        with Transaction().set_context(stock_date_end=self.start.date,
-                locations=[self.start.warehouse.id]):
+        context = {'show_stock': self.start.show_stock}
+        if self.start.date:
+            context['stock_date_end'] = self.start.date
+        if self.start.warehouse:
+            context['locations'] = [self.start.warehouse.id]
+
+        with Transaction().set_context(**context):
             return super(OpenBOMTree, self).default_tree(fields)
